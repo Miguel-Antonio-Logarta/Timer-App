a9ab4f38343d4566a302eb0a0d5960bacf661cfd
Miguel Antonio Logarta <mlogarta@students.solano.edu>
2021-10-20 15:50:07 -0700

    Added new components to be used

---
+++ src/components/Data.js
@@ -0,0 +1,10 @@
+const timers = {
+    "pomodoro": 1500000,
+    "long_break": 900000,
+    "short_break": 300000
+}
+
+const stying = {
+    background_url: "#",
+    bacground_blur: 0,
+}
---
+++ src/components/Todos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Todos extends React.Component {
+    render() {
+        return <div>This is the Todos window</div>;
+    }
+}
+
+export default Todos;
